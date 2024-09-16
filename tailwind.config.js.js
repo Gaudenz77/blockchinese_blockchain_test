536d2c063ce2b071618944ccdf2ef02b319ee220
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2024-09-16 21:55:50 +0200

    ud

--- tailwind.config.js.js
+++ tailwind.config.js.js
@@ -1,9 +1,15 @@
 module.exports = {
-  //...
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx,vue}",
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [
     require('daisyui'),
   ],
   daisyui: {
-    themes: ["light", "dark", "cyberpunk"],
+    themes: ["light", "dark", "cyberpunk"], // Ensure these themes are correctly installed
   },
 }
